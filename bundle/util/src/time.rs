0933157c803bf981c54448315d9b4c109385e6a4
Andrew Danger Lyon <orthecreedence@gmail.com>
2019-09-23 17:41:37 -0700

    updating time util a bit

--- bundle/util/src/time.rs
+++ bundle/util/src/time.rs
@@ -1,7 +1,11 @@
 use chrono::{DateTime, Utc, NaiveDateTime};
 
-pub fn is_current(now: &DateTime<Utc>) -> bool {
-    return (Utc::now() - now.clone()).num_seconds() < 10;
+pub fn now() -> DateTime<Utc> {
+    Utc::now()
+}
+
+pub fn is_current(time: &DateTime<Utc>) -> bool {
+    return (now() - time.clone()).num_seconds() < 10;
 }
 
 pub fn from_timestamp(ts: i64) -> DateTime<Utc> {
